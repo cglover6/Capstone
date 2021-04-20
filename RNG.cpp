c51fd66e950559a733f6c75dfec882bbd98732bf
LukeJanak <73982328+LukeJanak@users.noreply.github.com>
2021-04-20 17:29:15 -0500

    Adding the random number generator code

---
+++ RNG.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+int main()
+{
+	srand((unsigned) time(0));
+	//Whatever the skill is:
+	int skill = (rand() % 35) + 1;
+}
